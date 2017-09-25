06995ec434f3c27fd8c653909d9ce331ebf7be53
Elliot Lockerman <elliot@lockerman.info>
2017-09-25 18:42:41 -0400

    Initial Commit

---
+++ CaffineTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaffineTracker
+//
+//  Created by EL on 9/25/17.
+//  Copyright © 2017 el. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
