2e6cc67627e5dece1666fc07099c68c2b546aa1d
Elliot Lockerman <elliot@lockerman.info>
2017-10-25 19:48:11 -0400

    Added basic implementation of all complications.

--- CaffineTracker WatchKit Extension/ComplicationController.swift
+++ CaffineTracker WatchKit Extension/ComplicationController.swift
@@ -38,19 +38,59 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        if complication.family == .modularSmall {
-            if let controller = WKExtension.shared().rootInterfaceController as? InterfaceController {
-                let template = CLKComplicationTemplateModularSmallStackText()
-                template.line1TextProvider = CLKSimpleTextProvider(text: "\(controller.getAgoComplication(date: Date()))")
-                template.line2TextProvider = CLKSimpleTextProvider(text: "\(controller.getDoseComplication())")
-                
-                let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
-                handler(timelineEntry)
-                return;
-            }
-            
+        guard let controller = WKExtension.shared().rootInterfaceController as? InterfaceController else {
+            print("Root controller wasn't InterfaceController")
+            handler(nil)
+            return
         }
-        handler(nil)
+        
+        let date = CLKSimpleTextProvider(text: "\(controller.getAgoComplication(date: Date()))")
+        let dose = CLKSimpleTextProvider(text: "\(controller.getDoseComplication())")
+        
+        var template: CLKComplicationTemplate;
+        
+        switch complication.family {
+            
+        case .modularSmall:
+            let temp = CLKComplicationTemplateModularSmallStackText()
+            temp.line1TextProvider = date
+            temp.line2TextProvider = dose
+            template = temp
+            
+        case .modularLarge:
+            let temp = CLKComplicationTemplateModularLargeStandardBody()
+            temp.headerTextProvider = CLKSimpleTextProvider(text: "Today's Caffeine")
+            temp.body1TextProvider = CLKSimpleTextProvider(text: "\(controller.getAgo(date: Date()))")
+            temp.body2TextProvider = CLKSimpleTextProvider(text: "\(controller.getDoseComplication()) mg")
+            template = temp
+        
+        case .circularSmall:
+            let temp = CLKComplicationTemplateCircularSmallStackText()
+            temp.line1TextProvider = date
+            temp.line2TextProvider = dose
+            template = temp
+            
+        case .extraLarge:
+            let temp = CLKComplicationTemplateExtraLargeStackText()
+            temp.line1TextProvider = date
+            temp.line2TextProvider = dose
+            template = temp
+            
+        case .utilitarianSmall: fallthrough
+        case .utilitarianSmallFlat:
+            let temp = CLKComplicationTemplateUtilitarianSmallFlat()
+            temp.textProvider = CLKSimpleTextProvider(text: "\(controller.getAgoComplication(date: Date()))/\(controller.getDoseComplication())")
+            template = temp
+        
+        case .utilitarianLarge:
+            let temp = CLKComplicationTemplateUtilitarianLargeFlat()
+            temp.textProvider = CLKSimpleTextProvider(text: "\(controller.getAgoComplication(date: Date())) / \(controller.getDoseComplication()) mg")
+            template = temp
+        }
+        
+        
+        let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+        handler(timelineEntry)
     }
     
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -60,24 +100,28 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         // Call the handler with the timeline entries after to the given date
-        if complication.family == .modularSmall {
-            if let controller = WKExtension.shared().rootInterfaceController as? InterfaceController {
-                let loops = min(limit, 120)
-                var arr = [CLKComplicationTimelineEntry]()
-                for i in 0 ..< loops {
-                    let date2 = date.addingTimeInterval(TimeInterval(i * (60)))
-                    let template = CLKComplicationTemplateModularSmallStackText()
-                    template.line1TextProvider = CLKSimpleTextProvider(text: "\(controller.getAgoComplication(date: date2))")
-                    template.line2TextProvider = CLKSimpleTextProvider(text: "\(controller.getDoseComplication())")
-                    
-                    let timelineEntry = CLKComplicationTimelineEntry(date: date2, complicationTemplate: template)
-                    arr.append(timelineEntry)
-                }
-                handler(arr)
-                return;
-            }
-            
+        guard let controller = WKExtension.shared().rootInterfaceController as? InterfaceController else {
+            handler(nil)
+            return
         }
+
+        if complication.family == .modularSmall {
+            let loops = min(limit, 120)
+            var arr = [CLKComplicationTimelineEntry]()
+            for i in 0 ..< loops {
+                let date2 = date.addingTimeInterval(TimeInterval(i * (60)))
+                let template = CLKComplicationTemplateModularSmallStackText()
+                template.line1TextProvider = CLKSimpleTextProvider(text: "\(controller.getAgoComplication(date: date2))")
+                template.line2TextProvider = CLKSimpleTextProvider(text: "\(controller.getDoseComplication())")
+                
+                let timelineEntry = CLKComplicationTimelineEntry(date: date2, complicationTemplate: template)
+                arr.append(timelineEntry)
+            }
+            handler(arr)
+            return;
+        }
+        
+        
         handler(nil)
     }
     
